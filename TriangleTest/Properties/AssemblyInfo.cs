947fb6b53dbc750fc56ce527c2d82514c356c411
Eunah Kim <kasmisowoo@naver.com>
2023-02-15 12:10:01 -0500

    test equialateral

---
+++ TriangleTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TriangleTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TriangleTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ded5674a-2336-4faf-be98-ab52c6ac4741")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TriangleTest/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Security.Policy;
+using TriangleSolver;
+
+namespace TriangleTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //- Only one(1) test for a valid equilateral triangle
+        [TestMethod]
+        public void AnalyzeTriangle_f10s10t10_EQUILATERAL()
+        {
+            // Arrange
+            int firstSide = 10;
+            int secondSide = 10;
+            int thirdSide = 10;
+            string output = "Based on all sides being equal, the type of triangle is an EQUILATERAL";
+
+            // Act
+            Triangle triagle = new Triangle();
+            string actual = triagle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(output, actual);
+        }
+
+    }
+}
+
+
